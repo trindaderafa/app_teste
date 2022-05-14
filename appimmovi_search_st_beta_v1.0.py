5d35a34b205be45dba62a27ca6298e7198d510d3
Rafael Trindade <trindade.rafa@gmail.com>
2022-05-13 21:43:35 -0300

    Testando mapa pydeck estatico - html
    Gerando mapa dinamico 3D usando a biblioteca pydeck
    e exportando o resultado em um arquivo estatico em html.

--- appimmovi_search_st_beta_v1.0.py
+++ appimmovi_search_st_beta_v1.0.py
@@ -11,8 +11,8 @@ file_vivareal = 'bd_salvador.csv'
 #definindo a função para alterar o label da coluna de coordenadas
 # com os nomes latitude e longitude para lat e lon
 def load_data():
-    columns = {'Latitude': 'lat',
-               'Longitude': 'lon'}
+    columns = {'Latitude': 'latitude',
+               'Longitude': 'longitude'}
     df_vivareal = pd.read_csv(file_vivareal)
     df_vivareal = df_vivareal.rename(columns=columns)
     return df_vivareal
@@ -31,11 +31,11 @@ st.markdown(
 #criando a barra lateral e nomeando ela
 st.sidebar.header('Filtros')
 
-price_min = 10000
-price_max = 12000
+bedroom_min = 0
+bedroom_max = 4
 
-#criando um slider para filtrar preços de aluguel
-price = st.sidebar.slider('Veja os imóveis pelo valor de mercado', price_min, price_max)
+#criando um slider para filtrar numero de quartos
+bedrooms = st.sidebar.slider('Veja os imóveis pelo número de quartos', bedroom_min, bedroom_max)
 
 #criando uma lista para selecionar o tipo de imovel
 property_types = df_vivareal.Tipo.unique()
@@ -44,7 +44,7 @@ if not property_types_selected:
     property_types_selected = df_vivareal.Tipo.unique()
 
 #mostrando a tabela dos dados do Vivareal
-data_map = df_vivareal[(df_vivareal['Preço(R$)'] == price) & (df_vivareal['Tipo'].isin(property_types_selected))]
+data_map = df_vivareal[(df_vivareal['Quartos'] == bedrooms) & (df_vivareal['Tipo'].isin(property_types_selected))]
 st.dataframe(data_map)
 
 #criando um checkbox para mostrar/ocultar  mapa dos dados do Vivareal
@@ -53,34 +53,29 @@ if st.sidebar.checkbox('Mostrar Mapa'):
     map_vivareal = st.map(data_map)
 
 
-#criando um segundo mapa para mostrar grafico de preços de mercado
-#titulo do mapa
-st.markdown('### Mapa de Preços de Mercado')
-#usando biblioteca pydeck para gerar o mapa
-st.pydeck_chart(pdk.Deck(
-     #estilo do mapa
-     map_style='mapbox://styles/mapbox/light-v9',
-     #visualização padrão quando carregar o mapa (Madrid)
-     initial_view_state={
-         "latitude":-12.975056605825293,
-         "longitude":-38.50146502854858,
-         "zoom":10,
-         "pitch":0,
-        },
-     #visualização dos preços a partir das coordenadas
-     layers=[
-         pdk.Layer(
-            'HexagonLayer',
-            #carregando o arquivo em csv
-            data=file_vivareal,
-            #coordenadas tem que ser no mesmo nome da coluna do csv
-            get_position=['longitude', 'latitude'],
-            radius=35,
-            elevation_scale=4,
-            elevation_range=[price_min, price_max],
-            pickable=True,
-            extruded=True,
-         ),
+df = data_map
 
-     ],
- ))
+# Define a layer to display on a map
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    df,
+    pickable=True,
+    opacity=0.8,
+    stroked=True,
+    filled=True,
+    radius_scale=6,
+    radius_min_pixels=1,
+    radius_max_pixels=100,
+    line_width_min_pixels=1,
+    get_position=['longitude', 'latitude'],
+    get_radius="exits_radius",
+    get_fill_color=[25, 74, 215],
+    get_line_color=[0, 0, 0],
+)
+
+# Set the viewport location
+view_state = pdk.ViewState(latitude=-12.975056605825293, longitude=-38.50146502854858, zoom=10, bearing=0, pitch=0)
+
+# Render
+r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{Item}\n{Tipo}"})
+r.to_html("scatterplot_layer_1.html")
