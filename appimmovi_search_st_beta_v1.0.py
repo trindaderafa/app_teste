1af5f8dedf9751b368fba373f90460bfbb379a5e
Rafael Trindade <trindade.rafa@gmail.com>
2022-05-15 23:01:09 -0300

    Mudando o slider de quartos e ajustando multiselect
    Slider de quartos alterado para multiselect.
    Criado multiselect para os bairros e ordenado
    os itens da lista em ordem alfabetica.

--- appimmovi_search_st_beta_v1.0.py
+++ appimmovi_search_st_beta_v1.0.py
@@ -40,25 +40,43 @@ bedroom_min = 0
 bedroom_max = 4
 
 
-#criando um slider para filtrar numero de quartos
-bedrooms = st.sidebar.slider('Veja os imóveis pelo número de quartos', bedroom_min, bedroom_max)
-
+#criando uma lista para filtrar numero de quartos
+#usando sort_values() do pandas para classificar em ordem alfabetica
+bedrooms = df_vivareal.sort_values(by='Quartos').Quartos.unique()
+bedrooms_selected = st.sidebar.multiselect('Veja os imóveis pelo número de quartos', bedrooms)
 
 #criando uma lista para selecionar o tipo de imovel
-property_types = df_vivareal.Tipo.unique()
+property_types = df_vivareal.sort_values(by='Tipo').Tipo.unique()
 property_types_selected = st.sidebar.multiselect('Selecione o tipo de imóvel', property_types)
+
+#criando uma lista para selecionar o bairro do imovel
+property_neighborhoods = df_vivareal.sort_values(by='Bairro').Bairro.unique()
+property_neighborhoods_selected = st.sidebar.multiselect('Selecione o bairro do imóvel', property_neighborhoods)
+
+
+#condicional para o caso do multiselect não estar selecionado
+#deixar mostrando os bairros no dataframe
+if not bedrooms_selected:
+    bedrooms_selected = df_vivareal.Quartos.unique()
+
 if not property_types_selected:
     property_types_selected = df_vivareal.Tipo.unique()
 
+if not property_neighborhoods_selected:
+    property_neighborhoods_selected = df_vivareal.Bairro.unique()
 
-#mostrando a tabela dos dados do Vivareal
-data_map = df_vivareal[(df_vivareal['Quartos'] == bedrooms) & (df_vivareal['Tipo'].isin(property_types_selected))]
+#mostrando o dataframe dos dados do Vivareal
+data_map = df_vivareal[(df_vivareal['Quartos'].isin(bedrooms_selected)) &
+                       (df_vivareal['Tipo'].isin(property_types_selected)) &
+                       (df_vivareal['Bairro'].isin(property_neighborhoods_selected))]
 st.dataframe(data_map)
 
 
 #criando novo dataframe para exibir dados especificos no mapa (pydeck)
-df = df_vivareal[['Item', 'Tipo', 'latitude', 'longitude', 'Preço/m2', 'Quartos']]
-data_map_1 = df[(df['Quartos'] == bedrooms) & (df['Tipo'].isin(property_types_selected))]
+df = df_vivareal[['Item', 'Tipo', 'Bairro', 'latitude', 'longitude', 'Preço/m2', 'Quartos']]
+data_map_1 = df[(df['Quartos'].isin(bedrooms_selected)) &
+                (df['Tipo'].isin(property_types_selected)) &
+                (df['Bairro'].isin(property_neighborhoods_selected))]
 
 
 #definindo o layer para mostrar no mapa
@@ -69,7 +87,7 @@ layer = pdk.Layer(
     opacity=0.8,
     stroked=True,
     filled=True,
-    radius_scale=120,
+    radius_scale=60,
     radius_min_pixels=1,
     radius_max_pixels=100,
     line_width_min_pixels=1,
@@ -90,4 +108,5 @@ if st.sidebar.checkbox('Mostrar Mapa'):
     st.markdown('### Mapa de Dados')
     map_vivareal = st.pydeck_chart(pdk.Deck(layers=[layer], map_style='mapbox://styles/mapbox/light-v10',
                   initial_view_state=view_state, height=500, width='100%',
-                         views=[pdk.View(type="MapView", controller=True)],tooltip={"text": "{Item}\n{Tipo}"}))
+                        views=[pdk.View(type="MapView", controller=True)],
+                            tooltip={"text": "{Item}\n{Tipo}\n{Bairro}\nR$ {Preço/m2}/m²\n{latitude} {longitude}"}))
