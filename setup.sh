da2408e052b36f74c35188966ea470e00c910cf7
Rafael Trindade <trindade.rafa@gmail.com>
2022-06-07 13:55:54 -0300

    7a tentativa de deploy no heroku
    Eliminando linhas sobre tema e mapbox do 'config.toml'

--- setup.sh
+++ setup.sh
@@ -4,10 +4,4 @@ echo "[server]
 headless = true
 port = $PORT
 enableCORS = false
-[theme]
-base = 'light'
-primaryColor = '#194ad7'
-backgroundColor = '#ecf4fb'
-[mapbox]
-token = 'pk.eyJ1IjoiYXBwaW1tb3ZpIiwiYSI6ImNsMzR6eXg4djE4bjMzanJ4NzVic2Nmb3EifQ.HgHvze4WT9SB9WYG6GTZmg'
 " > ~/.streamlit/config.toml
