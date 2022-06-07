c30209a5ceed78acdf080b03677013e5169bd9cf
Rafael Trindade <trindade.rafa@gmail.com>
2022-06-07 14:03:38 -0300

    8a tentativa de deploy no heroku
    Alterando o arquivo 'setup.sh' para o original exceto
    criacao do arquivo 'credentials.toml'

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-mkdir -p ~/.streamlit
-
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
